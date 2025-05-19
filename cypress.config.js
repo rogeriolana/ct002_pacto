260060e1040caba4bc8ed7d9f6e9e6ba4eeeb0b7
rogerio <rogeriolana9@gmail.com>
2025-05-19 10:02:49 -0300

    fix: rm gravacao video

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true, // grava vídeo por padrão
   e2e: {
     setupNodeEvents(on, config) {
       // implementa plugins, se necessário
